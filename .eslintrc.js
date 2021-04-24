100bbabdfd89abdceb7d5ce7aeacbbcfef785a43
Tachibana Kiyomi <me@yuru.me>
2021-04-24 22:06:29 +0900

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,19 +18,22 @@ module.exports = {
     },
   ],
   rules: {
+    // https://eslint.org/docs/rules/
+    'no-empty': 'off',
+    // https://github.com/facebook/react/tree/master/packages/eslint-plugin-react-hooks
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    // https://palantir.github.io/tslint/rules/
     '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    // https://github.com/yannickcr/eslint-plugin-react#list-of-supported-rules
     'react/prop-types': 'off',
     'react/display-name': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
-    'no-empty': 'off',
   },
 };
